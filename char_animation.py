97c94f8c176d010ed893db49ac64bec41707f5d6
Quites <andersonkpetry@gmail.com>
2022-03-02 10:27:20 -0300

    Enviado char_animation.py

---
+++ char_animation.py
@@ -0,0 +1,9 @@
+from sys import stdout
+import time
+#--------------------------------------------------------------------------------
+def anim(text):
+    words = f"\n{str(text)}\n"
+    for char in words:
+        time.sleep(0.1)
+        stdout.write(char)
+        stdout.flush()
