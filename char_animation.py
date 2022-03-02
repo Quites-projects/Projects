4cb7f6e2cb5f707d1680e7c88acbc7dfa9527f7e
Quites <andersonkpetry@gmail.com>
2022-03-02 10:37:10 -0300

    adicionando comentarios para facil entendimento

--- char_animation.py
+++ char_animation.py
@@ -4,6 +4,8 @@ import time
 def anim(text):
     words = f"\n{str(text)}\n"
     for char in words:
-        time.sleep(0.1)
+        time.sleep(0.1) # mude o tempo de acordo com a sua preferencia
         stdout.write(char)
         stdout.flush()
+
+#use anim("escreva o texto aqui")
